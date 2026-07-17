ddacc3d7cd663efca395e142416df598d344b177
Aniruddha <dairymitraofficial@gmail.com>
2026-07-17 16:58:59 +0530

    Use Firebase credentials from environment variable

--- utils/push.py
+++ utils/push.py
@@ -1,10 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-# 🔐 INIT FIREBASE ONLY ONCE
+# Initialize Firebase only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_service_account.json")
+    firebase_json = json.loads(os.environ["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(firebase_json)
     firebase_admin.initialize_app(cred)
+
 
 def send_push(token, title, body, url="/"):
     """
@@ -21,4 +25,4 @@ def send_push(token, title, body, url="/"):
         token=token
     )
 
-    messaging.send(message)
+    messaging.send(message)git add .
